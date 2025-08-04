f0bb3c67910a365ba97fecb384def4f1ccffbdaf
Jonas <git@deportedinto.space>
2025-08-04 23:18:17 +0200

    (almost) sane version. - Added content collections - Theming - Base Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://deportedinto.space",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [preact()],
+});
